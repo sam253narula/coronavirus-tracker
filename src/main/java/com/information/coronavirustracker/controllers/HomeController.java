007d84df0542acb732b08a7b6b6a959b370582d5
sam253narula <samarthnarula253@gmail.com>
2020-03-29 20:47:11 +0530

    "Introducing Lombok to improve quality of code"

--- src/main/java/com/information/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/information/coronavirustracker/controllers/HomeController.java
@@ -13,35 +13,33 @@ import java.util.List;
 @Controller
 public class HomeController {
 
-    @Autowired
-    CoronaVirusDataService coronaVirusDataService;
+	@Autowired
+	CoronaVirusDataService coronaVirusDataService;
 
-    @GetMapping("/")
-    public String home(Model model) {
-        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
-        List<LocationStats> indianStats = coronaVirusDataService.getIndianStats();
-        //Mapping Indian data below:
-        int totalReportedCasesInIndia = indianStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
-        int totalNewCasesInIndia = indianStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
-        
-        //Mapping World data below:
-        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
-        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
-        
-        
-        model.addAttribute("locationStats", allStats);
-        model.addAttribute("indianStats", indianStats);
-      
-        //world data below:
-        model.addAttribute("totalReportedCases", totalReportedCases);
-        model.addAttribute("totalNewCases", totalNewCases);
-        
-        //India data below:
-        model.addAttribute("totalReportedCasesInIndia", totalReportedCasesInIndia);
-        model.addAttribute("totalNewCasesInIndia", totalNewCasesInIndia);
+	@GetMapping("/")
+	public String home(Model model) {
+		List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+		List<LocationStats> indianStats = coronaVirusDataService.getIndianStats();
+		// Mapping Indian data below:
+		int totalReportedCasesInIndia = indianStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+		int totalNewCasesInIndia = indianStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
 
-        return "home";
-    }
-    
-    
+		// Mapping World data below:
+		int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+		int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+
+		model.addAttribute("locationStats", allStats);
+		model.addAttribute("indianStats", indianStats);
+
+		// world data below:
+		model.addAttribute("totalReportedCases", totalReportedCases);
+		model.addAttribute("totalNewCases", totalNewCases);
+
+		// India data below:
+		model.addAttribute("totalReportedCasesInIndia", totalReportedCasesInIndia);
+		model.addAttribute("totalNewCasesInIndia", totalNewCasesInIndia);
+
+		return "home";
+	}
+
 }
